76a335d31a90b46eea8139416455a0afe01231e1
Matt Owen <mattjowen1991@gmail.com>
2026-03-01 11:52:11 +0000

    Manual CSS revert

--- www/assets/index-WLFpOuj8.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-2yQxOP0Y.js","assets/index-BvwD2HY5.js","assets/index-CwURKJsu.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-BvwD2HY5.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-2yQxOP0Y.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
