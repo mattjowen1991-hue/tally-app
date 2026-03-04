6ccb9d4dac0b0d31684e9fc9aba4c34395966dfb
Matt Owen <mattjowen1991@gmail.com>
2026-03-04 22:36:14 +0000

    Fix donut chart: use object borderRadius for all four arc corners

--- www/assets/index-8WVVGdeI.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BOQT1jd0.js","assets/index-B-RJswza.js","assets/index-DBI8EwEJ.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-B-RJswza.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-BOQT1jd0.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
