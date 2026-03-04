f4f06afb0fd0696c672d2de69a1e1a4fd34d576b
Matt Owen <mattjowen1991@gmail.com>
2026-03-04 22:49:50 +0000

    Fix donut chart: remove hoverOffset to prevent flat edge artifact on tap

---
+++ www/assets/index-B-MxrENZ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Di6w3Qg3.js","assets/index-CfjZE56S.js","assets/index-DBI8EwEJ.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-CfjZE56S.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-Di6w3Qg3.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
