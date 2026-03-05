d69b6847d74709b96b19c14593fadef38f42e0ed
Matt Owen <mattjowen1991@gmail.com>
2026-03-05 10:13:20 +0000

    Fix bottom bar: match navigation bar color to app background

--- www/assets/index-C4R3L6hv.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BHG75GCq.js","assets/index-YCAtxfbY.js","assets/index-DBI8EwEJ.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-YCAtxfbY.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-BHG75GCq.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
