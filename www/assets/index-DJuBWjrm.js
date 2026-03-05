47dac30b4310a308a01e9bdf08874ad18a2d7580
Matt Owen <mattjowen1991@gmail.com>
2026-03-05 10:22:11 +0000

    Fix status bar: match app background color

--- www/assets/index-DJuBWjrm.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-maRYtoaO.js","assets/index-B4IfwJqJ.js","assets/index-sfifTxyu.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-B4IfwJqJ.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-maRYtoaO.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
