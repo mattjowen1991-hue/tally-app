6d1cf60e4dc292776c3fde5f5699e987b7292cc4
Matt Owen <mattjowen1991@gmail.com>
2026-03-07 14:35:34 +0000

    Fix status bar: remove system window inset gap in activity_main layout

--- www/assets/index-BbYpCRtQ.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DDkvC0MS.js","assets/index-BtqFQcZ0.js","assets/index-O5Zh42-7.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-BtqFQcZ0.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-DDkvC0MS.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
