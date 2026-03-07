2d9e83ec27b354ef260501979a2a125612abffca
Matt Owen <mattjowen1991@gmail.com>
2026-03-07 14:46:11 +0000

    Fix grey strip: force edge-to-edge via WindowCompat in MainActivity

--- android/app/src/main/java/com/tally/billtracker/MainActivity.java
+++ android/app/src/main/java/com/tally/billtracker/MainActivity.java
@@ -1,7 +1,12 @@
 package com.tally.billtracker;
-
 import android.os.Bundle;
+import android.view.WindowManager;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
-
 public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
 }
