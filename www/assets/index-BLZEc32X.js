eb19f7bb5a5dd1416e17056a6c5b3e84695ebbc2
Matt Owen <mattjowen1991@gmail.com>
2026-04-04 13:22:22 +0100

    Checkpoint: before status bar first-load fix

--- www/assets/index-BLZEc32X.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CYc1tBo2.js","assets/index-Bj7WdKz5.js","assets/index-O5Zh42-7.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-Bj7WdKz5.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-CYc1tBo2.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
