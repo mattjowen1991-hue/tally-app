a20146734f06458fab95a8fb07e182b94691eabd
Matt Owen <mattjowen1991@gmail.com>
2026-04-07 00:09:17 +0100

    Debug: log focusin events

--- src/utils/keyboardScroll.js
+++ src/utils/keyboardScroll.js
@@ -1,11 +1,12 @@
 export function initKeyboardScroll() {
   document.addEventListener('focusin', (e) => {
     const el = e.target;
+    console.log('[KB] focusin:', el.tagName, el.name || el.placeholder || 'no-id', 'inModal:', !!el.closest('.modal-content'));
     if (!el.closest('.modal-content')) return;
     if (el.tagName !== 'INPUT' && el.tagName !== 'TEXTAREA') return;
 
-    // Wait for keyboard to fully open then scroll field into view
     setTimeout(() => {
+      console.log('[KB] scrollIntoView firing for:', el.tagName, el.placeholder);
       el.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }, 350);
   }, true);
--- www/assets/index-BBCddgPx.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BS8LcGtM.js","assets/index-CUukS4FG.js","assets/index-B1hps5Yn.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-CUukS4FG.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-BS8LcGtM.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
