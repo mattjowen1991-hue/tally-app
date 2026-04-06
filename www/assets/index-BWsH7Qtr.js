7fed5016765f84c3aab8f344129333eed550ace8
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 22:20:51 +0100

    Fix: adjustResize in AndroidManifest - native keyboard handling

---
+++ www/assets/index-BWsH7Qtr.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-z1XRjqoR.js","assets/index-BER3q9r5.js","assets/index-rZ8l5khI.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-BER3q9r5.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-z1XRjqoR.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
