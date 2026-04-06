a69d707b8a8fcc3bc7fbd67d4a923cdcdc3a21aa
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 21:59:10 +0100

    Fix: simple scrollable modal, no JS keyboard tricks, padding-bottom for keyboard space

---
+++ www/assets/index-s-MCLAXc.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-yKW2XBn6.js","assets/index-DhDqoUWo.js","assets/index-BVAplDGp.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-DhDqoUWo.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-yKW2XBn6.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
