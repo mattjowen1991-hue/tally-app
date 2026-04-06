2a38b77fe937d1629a45effe66049b1b7aaf26ab
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 15:42:39 +0100

    Fix: disable text selection and copy menu throughout app

--- www/assets/index-B2qSrv9q.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BEwdAa5f.js","assets/index-4vpcw-BF.js","assets/index-DGFqZwxG.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-4vpcw-BF.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-BEwdAa5f.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
---
+++ www/assets/index-JfJnNlYw.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BT8gQKBr.js","assets/index-8sFHL896.js","assets/index-DGFqZwxG.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-8sFHL896.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-BT8gQKBr.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
