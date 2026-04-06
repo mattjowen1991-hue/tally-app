ec94acdd094613bebc1871e1e7f3e0dc27051763
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 21:58:33 +0100

    Fix: simple scrollable modal, no JS keyboard tricks, padding-bottom for keyboard space

---
+++ www/assets/index-BNCZWHwC.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CeD7hCaf.js","assets/index-ifo79j7H.js","assets/index-DvX2XJ-L.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-ifo79j7H.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-CeD7hCaf.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
--- www/assets/index-DUZt6iSF.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BXCun1KT.js","assets/index-CLn82gUS.js","assets/index-72HPj1r6.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-CLn82gUS.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-BXCun1KT.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
