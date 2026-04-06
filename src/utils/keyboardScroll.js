f0795533d1fd0b356010f7232dafc67e4edb01bc
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 23:20:01 +0100

    Fix: use keyboardDidShow + visualViewport to eliminate gap

--- src/utils/keyboardScroll.js
+++ src/utils/keyboardScroll.js
@@ -1,10 +1,23 @@
 import { Keyboard } from '@capacitor/keyboard';
 
 export function initKeyboardScroll() {
-  Keyboard.addListener('keyboardWillShow', (info) => {
+  // Try both events - didShow fires after keyboard is fully open
+  Keyboard.addListener('keyboardDidShow', (info) => {
     document.documentElement.style.setProperty('--keyboard-height', `${info.keyboardHeight}px`);
   });
   Keyboard.addListener('keyboardWillHide', () => {
     document.documentElement.style.setProperty('--keyboard-height', '0px');
   });
+
+  // Also measure via visualViewport as a reliable fallback
+  if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', () => {
+      const gap = window.innerHeight - window.visualViewport.height - window.visualViewport.offsetTop;
+      if (gap > 100) {
+        document.documentElement.style.setProperty('--keyboard-height', `${gap}px`);
+      } else {
+        document.documentElement.style.setProperty('--keyboard-height', '0px');
+      }
+    });
+  }
 }
---
+++ www/assets/index-DuxQrLQV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bsxws7DI.js","assets/index--vUK0z0x.js","assets/index-BaFeAVKF.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index--vUK0z0x.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-Bsxws7DI.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
