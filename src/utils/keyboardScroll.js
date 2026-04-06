af0b4c213d91956621448fcaeb7342e5b2051c9e
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 23:22:00 +0100

    Fix: keyboardDidShow only, remove visualViewport

--- src/utils/keyboardScroll.js
+++ src/utils/keyboardScroll.js
@@ -1,23 +1,10 @@
 import { Keyboard } from '@capacitor/keyboard';
 
 export function initKeyboardScroll() {
-  // Try both events - didShow fires after keyboard is fully open
   Keyboard.addListener('keyboardDidShow', (info) => {
     document.documentElement.style.setProperty('--keyboard-height', `${info.keyboardHeight}px`);
   });
-  Keyboard.addListener('keyboardWillHide', () => {
+  Keyboard.addListener('keyboardDidHide', () => {
     document.documentElement.style.setProperty('--keyboard-height', '0px');
   });
-
-  // Also measure via visualViewport as a reliable fallback
-  if (window.visualViewport) {
-    window.visualViewport.addEventListener('resize', () => {
-      const gap = window.innerHeight - window.visualViewport.height - window.visualViewport.offsetTop;
-      if (gap > 100) {
-        document.documentElement.style.setProperty('--keyboard-height', `${gap}px`);
-      } else {
-        document.documentElement.style.setProperty('--keyboard-height', '0px');
-      }
-    });
-  }
 }
---
+++ www/assets/index-D9c5bKAN.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B5ZzycHq.js","assets/index-BM1SDiHY.js","assets/index-BaFeAVKF.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-BM1SDiHY.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-B5ZzycHq.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
