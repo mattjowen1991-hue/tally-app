0b4ee59dd73f7a2b3ce13f332bad16edba8e47bd
Matt Owen <mattjowen1991@gmail.com>
2026-04-06 23:05:50 +0100

    Fix: modal respects status bar, padding-bottom for keyboard scroll room

---
+++ www/assets/index-DKs1HM87.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C9rBXhWn.js","assets/index-buhESI9-.js","assets/index-Ctr_NKGT.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-buhESI9-.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-C9rBXhWn.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
